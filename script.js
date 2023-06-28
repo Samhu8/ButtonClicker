7fd823cbe6c5db926635136f8ca197886fb115bb
Hugo Samayoa <hsamayoa07@gmail.com>
2023-06-27 17:59:34 -0700

    Adding Javascript functions

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function loginChange(element){
+    if (element.innerText == "Login") {
+        element.innerText = "Logout"
+    }
+    else {
+        element.innerText = "Login"
+    }
+}
+
+function disappear(element){
+    element.remove();
+}
+
+function likes(){
+    alert("Ninja was liked")
+}
+
+
+
+
+
